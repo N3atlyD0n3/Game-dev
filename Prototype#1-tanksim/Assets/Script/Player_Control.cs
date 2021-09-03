1f305676c279050991a8d4c59a6a14d8c77d9408
Jay <jay.m169@stu.nebo.edu>
2021-09-03 14:19:53 -0600

    Added Movement!
    Added movement to the tank

--- Prototype#1-tanksim/Assets/Script/Player_Control.cs
+++ Prototype#1-tanksim/Assets/Script/Player_Control.cs
@@ -7,7 +7,8 @@ public class Player_Control : MonoBehaviour
     
     public float speed; 
     public float turn_speed;
-
+    private float hInput;
+    private float vInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,14 @@ public class Player_Control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        //Get horivontal and Vertical input from unity
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        //Moves Tank forward and back
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput);
+        //Move Tank right and left
+        transform.Rotate(Vector3.up, turn_speed * hInput * Time.deltaTime );
+
     }
 }
