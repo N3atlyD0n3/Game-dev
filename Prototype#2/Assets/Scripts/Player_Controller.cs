a70567516ae10015c29d0f678bc5372bc5357740
Jay <jay.m169@stu.nebo.edu>
2021-09-21 14:21:35 -0600

    Added barriers and scripts to objects

--- Prototype#2/Assets/Scripts/Player_Controller.cs
+++ Prototype#2/Assets/Scripts/Player_Controller.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    public float speed = 15.0f;
-    public float turnSpeed = 15.0f;
+    //set player speed and turn speed
+    public float speed = 40.0f;
+    public float turnSpeed = 200f;
+    //set player input 
     private float hInput;
     private float vInput;
+    //set max y and x range player can move
+    public float xRange = 10.025f;
+    public float yRange = 4.49f; 
     // Update is called once per frame
     void Update()
     {
@@ -15,8 +20,28 @@ public class Player_Controller : MonoBehaviour
         hInput = Input.GetAxis("Horizontal");
         vInput = Input.GetAxis("Vertical");
         // Makes Player Character rotate left and right
-        transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime * vInput);
+        transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime * hInput);
         //Makes Player Character Move Forward and Backward
-        transform.Translate(Vector3.forward * speed * Time.deltaTime * hInput);
+        transform.Translate(Vector3.up * speed * Time.deltaTime * vInput);
+        //if player is past max range for x or y it stops the player
+            // X
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+            // Y
+        if(transform.position.y > yRange)
+        {
+            transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
+        }
+        if(transform.position.y < -yRange)
+        {
+            transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
+        }
+
     }
 }
