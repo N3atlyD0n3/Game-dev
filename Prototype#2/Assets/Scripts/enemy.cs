f79909502f615753fe5d87094d13b3df26c0d733
Jay <jay.m169@stu.nebo.edu>
2021-09-27 13:00:47 -0600

    Added enemy "AI".
    Added to the enemy script to follow the player.

--- Prototype#2/Assets/Scripts/enemy.cs
+++ Prototype#2/Assets/Scripts/enemy.cs
@@ -2,12 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemy : MonoBehaviour
-{// Start is called before the first frame update
-    void Start(){    
+public class enemy : MonoBehaviour{
+    // Start is called before the first frame update
+    public Transform player;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    void Start(){  
+        rb = this.GetComponent<Rigidbody2D>();  
     }
     // Update is called once per frame
     void Update(){
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+    private void FixedUpdate(){
+        moveCharacter(movement);
+    }
+    void moveCharacter(Vector2 direction){
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
     }
     void onTriggerEnter2D(Collider2D other){
         if(other.gameObject.CompareTag("Projectile")){
